9d01e14ecccca833213be1a547062dec48be40fa
Ash10-ap <atharvpatil2001@gmail.com>
2023-03-18 18:18:08 +0530

    server chage

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const __dirname = path.dirname(__filename)
 const app = express();
 
 //middelwares
-app.use(cors({ credentials: true, origin: 'https://balcony-backend.vercel.app/' }));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "./client/build")))
